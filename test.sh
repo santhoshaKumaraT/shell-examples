736a388740e6bae5bce8ffd5db0449d6b3ca86e6
Santhosha Kumar T <santhosha.kumar.t@ericsson.com>
2024-09-12 14:08:25 +0530

    commit

--- test.sh
+++ test.sh
@@ -3,17 +3,25 @@
 ##Sub
 #
 #Mul
-<<<<<<< HEAD
+
 #test123
 #Percent
+<<<<<<< HEAD
 <<<<<<< HEAD
 #test123456
 =======
 =======
+=======
+>>>>>>> 573858c (commit)
 
 
 
 
 
+<<<<<<< HEAD
 >>>>>>> 884e719 (commit)
 >>>>>>> 1cefecf (Fixed)
+=======
+
+
+>>>>>>> 573858c (commit)
